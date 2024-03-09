ebe01a3c22d6650401465a8559ee2e9211464681
Akib-Vijapura <akibv93@gmail.com>
2024-03-09 10:14:41 +0530

    some changes

--- main/hello.cpp
+++ main/hello.cpp
@@ -7,6 +7,6 @@ int main(void)
     cout << "Hello, World\n";
     cout << "Hello, Jenkins\n";
     cout << "I have successfully built and run\n";
-    cout << "Some changes with Task-2" << endl;
+    cout << "Some changes with Task-2 edit" << endl;
     return 0;
 }
